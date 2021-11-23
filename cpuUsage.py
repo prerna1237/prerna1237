6df02300301cbff79500a2c005f9c3e67bed3749
prerna1237 <37104186+prerna1237@users.noreply.github.com>
2021-11-23 10:04:26 +0530

    Update cpuUsage.py

--- cpuUsage.py
+++ cpuUsage.py
@@ -3,6 +3,9 @@ import psutil
 import smtplib
 import subprocess
 import sys
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 
 gmail_password = sys.argv[1]
 
@@ -11,16 +14,35 @@ cpuUsage = psutil.cpu_percent(10)
 
 ramUsage = psutil.virtual_memory()[2]
 
+
+msg = MIMEMultipart()
+msg['From'] = 'prerna.gupta7321@gmail.com'
+msg['To'] = 'prerna.gupta03@nagarro.com'
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login('prerna.gupta7321@gmail.com', gmail_password)
 
-if cpuUsage > 15:
+if cpuUsage > 10:
   print("CPU utilization is above the minimal value i.e. " + str(cpuUsage))
-  server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com', 'CPU utilization is above the minimal value i.e. ' + str(cpuUsage))
+
+  msg['Subject'] = 'Jenkins CPU Utilization above threshold'
+  body = 'CPU utilization is above the minimal value i.e. ' + str(cpuUsage)
+  msg.attach(MIMEText(body,'plain'))
+  text = msg.as_string()
+  server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com',text)
+
 else:
   print("CPU utilization is below the minimal value i.e. " + str(cpuUsage))
+
 if ramUsage > 10:
   print("Memory Usage is above the minimal value i.e. " + str(ramUsage))
-  server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com','Memory Usage is above the minimal value i.e. ' + str(ramUsage))
+
+  msg['Subject'] = 'Jenkins Memory Usage above threshold'
+  body = 'Memory Usage is above the minimal value i.e. ' + str(ramUsage)
+  msg.attach(MIMEText(body,'plain'))
+  text = msg.as_string()
+  server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com',text)
+
 else:
   print("Memory Usage is below the minimal value i.e. " + str(ramUsage) + "\n")
+
+
