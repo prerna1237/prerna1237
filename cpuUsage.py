4d14350a12b5af7dbc53fada292ec589f31836b1
prerna1237 <37104186+prerna1237@users.noreply.github.com>
2021-11-23 09:58:58 +0530

    Update cpuUsage.py

--- cpuUsage.py
+++ cpuUsage.py
@@ -14,12 +14,12 @@ ramUsage = psutil.virtual_memory()[2]
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login('prerna.gupta7321@gmail.com', gmail_password)
 
-if cpuUsage > 85:
+if cpuUsage > 15:
   print("CPU utilization is above the minimal value i.e. " + str(cpuUsage))
   server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com', 'CPU utilization is above the minimal value i.e. ' + str(cpuUsage))
 else:
   print("CPU utilization is below the minimal value i.e. " + str(cpuUsage))
-if ramUsage > 70:
+if ramUsage > 10:
   print("Memory Usage is above the minimal value i.e. " + str(ramUsage))
   server.sendmail('prerna.gupta7321@gmail.com','prerna.gupta03@nagarro.com','Memory Usage is above the minimal value i.e. ' + str(ramUsage))
 else:
